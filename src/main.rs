96967ff14a8efff2308bf7cb03645c51621887c0
deschambaultpatrick <deschambaultpatrick@gmail.com>
2025-06-14 11:02:21 -0400

    add yahoo API and also dividends calculation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use time::OffsetDateTime;
 use tokio_test;
 use yahoo_finance_api as yahoo;
 
+use futures::stream::{self, StreamExt};
 use serde::Deserialize;
 use std::{
     error::Error,
@@ -15,15 +16,53 @@ struct Tickers {
     symbols: Vec<String>,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[cfg(not(feature = "blocking"))]
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    use std::{collections::HashMap, hash::Hash};
+
+    use time::macros::datetime;
+    use yahoo_finance_api::YResponse;
+
     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("config");
 
     let settings = load_all_toml_from_dir(path)?.ok_or("No config files found.")?;
 
     let tickers: Tickers = settings.try_deserialize()?;
 
-    for t in tickers.symbols {
-        println!("Ticker : {}", t);
+    let conn = yahoo::YahooConnector::new().unwrap();
+    let start = datetime!(2000-07-25 00:00:00.00 UTC);
+    let end = datetime!(2024-11-01 00:00:00.00 UTC);
+
+    let y_responses: HashMap<_, _> = stream::iter(tickers.symbols.clone())
+        .then(|t| {
+            let conn = &conn;
+            async move {
+                match conn.get_quote_history(&t, start, end).await {
+                    Ok(hist) => Some((t, hist)),
+                    Err(_) => None,
+                }
+            }
+        })
+        .filter_map(|x| async move { x })
+        .collect()
+        .await;
+
+    let dividends: HashMap<_, _> = y_responses
+        .into_iter()
+        .filter_map(|(ticker, hist)| {
+            // If `dividends()` returns an error, skip this entry
+            let total = hist.dividends().ok()?.iter().map(|x| x.amount).sum::<f64>();
+            Some((ticker, total))
+        })
+        .collect();
+
+    // Print
+    for (ticker, dividend) in dividends {
+        println!(
+            "Ticker: {}, start time: {}, end time: {}, accumulated dividends: {}",
+            ticker, start, end, dividend
+        )
     }
 
     Ok(())
