a2d9b4171e3c9589bded9069981ff383242f4c8a
deschambaultpatrick <deschambaultpatrick@gmail.com>
2025-06-14 12:37:30 -0400

    stock struct buld

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
 use derive_builder::Builder;
+use futures::stream;
 use serde::{Deserialize, Deserializer};
 use time::OffsetDateTime;
+use yahoo_finance_api::YahooConnector;
 
 #[derive(Default, Builder, Debug)]
 #[builder(setter(into))]
@@ -11,7 +15,32 @@ pub struct Wallet {
 
 #[derive(Clone, Default, Debug, Builder)]
 pub struct Stock {
-    ticker: String,
-    quantity: i64,
+    pub ticker: String,
     time_of_buy: Option<OffsetDateTime>,
+}
+
+impl Stock {
+    pub async fn value(
+        &self,
+        end: OffsetDateTime,
+        conn: &YahooConnector,
+    ) -> Result<f64, Box<dyn std::error::Error>> {
+        match self.time_of_buy {
+            Some(start) => {
+                let ticker = &self.ticker;
+                let y_response = conn.get_quote_history(ticker, start, end).await?;
+
+                let first_price_closed = y_response
+                    .quotes()?
+                    .first()
+                    .ok_or("First element not found")?
+                    .close;
+
+                let last_price_closed = y_response.last_quote()?.close;
+
+                Ok(last_price_closed - first_price_closed)
+            }
+            None => Err("No start time defined.".into()),
+        }
+    }
 }
