60b0573164f57eefab5500a4b81506e758af4a9d
deschambaultpatrick <deschambaultpatrick@gmail.com>
2025-06-15 18:01:29 -0400

    add market data provider

---
+++ src/market_data_provider.rs
@@ -0,0 +1,52 @@
+use std::error::Error;
+
+use chrono::{DateTime, NaiveDate};
+use time::OffsetDateTime;
+use yahoo_finance_api::YahooConnector;
+
+use crate::stock::{Stock, StockOrigin};
+use chrono::{Duration, TimeZone, Utc};
+
+#[async_trait::async_trait]
+pub trait MarketDataProvider {
+    async fn get_price_at_date(
+        &self,
+        ticker: &str,
+        date: &OffsetDateTime,
+    ) -> Result<f64, Box<dyn Error>>;
+
+    async fn is_ticker_valid(&self, ticker: &str) -> Result<bool, Box<dyn Error>>;
+}
+
+#[async_trait::async_trait]
+impl MarketDataProvider for YahooConnector {
+    async fn get_price_at_date(
+        &self,
+        ticker: &str,
+        date: &OffsetDateTime,
+    ) -> Result<f64, Box<dyn Error>> {
+        // Plage de temps d'un jour autour de la date donnée
+        let start = *date;
+        let end = start + time::Duration::days(1);
+
+        let response = self
+            .get_quote_history(ticker, start, end)
+            .await
+            .map_err(|e| format!("Error fetching quote history: {}", e))?;
+
+        let quotes = response
+            .quotes()
+            .map_err(|e| format!("Failed to parse quotes: {}", e))?;
+
+        let quote = quotes
+            .last() // On suppose que le dernier quote du jour est celui de clôture
+            .ok_or("No price data available for the given date")?;
+
+        Ok(quote.close)
+    }
+
+    async fn is_ticker_valid(&self, ticker: &str) -> Result<bool, Box<dyn Error>> {
+        let response = self.get_latest_quotes(ticker, "1d").await;
+        Ok(response.is_ok())
+    }
+}
