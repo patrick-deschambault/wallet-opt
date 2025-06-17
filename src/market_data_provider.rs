16fe5b56d07fd6180321dc8e0403307e4556319f
deschambaultpatrick <deschambaultpatrick@gmail.com>
2025-06-17 10:02:40 -0400

    get dividend per share from YahooConnector

--- src/market_data_provider.rs
+++ src/market_data_provider.rs
@@ -12,6 +12,13 @@ pub trait MarketDataProvider {
     ) -> Result<f64, Box<dyn Error>>;
 
     async fn is_ticker_valid(&self, ticker: &str) -> Result<bool, Box<dyn Error>>;
+
+    async fn get_dividends_per_share(
+        &self,
+        ticker: &str,
+        start: &OffsetDateTime,
+        end: &OffsetDateTime,
+    ) -> Result<Vec<(u64, f64)>, Box<dyn std::error::Error>>;
 }
 
 #[async_trait::async_trait]
@@ -44,5 +51,22 @@ impl MarketDataProvider for YahooConnector {
     async fn is_ticker_valid(&self, ticker: &str) -> Result<bool, Box<dyn Error>> {
         let response = self.get_latest_quotes(ticker, "1d").await;
         Ok(response.is_ok())
+    }
+
+    async fn get_dividends_per_share(
+        &self,
+        ticker: &str,
+        start: &OffsetDateTime,
+        end: &OffsetDateTime,
+    ) -> Result<Vec<(u64, f64)>, Box<dyn std::error::Error>> {
+        let response = self.get_quote_history(ticker, *start, *end).await?;
+        let dividends = response.dividends()?; // Hypothétique
+
+        let data = dividends
+            .into_iter()
+            .map(|entry| (entry.date, entry.amount))
+            .collect();
+
+        Ok(data)
     }
 }
